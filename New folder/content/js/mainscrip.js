206e20305ea426edb0617c53e73dcf8f484d4314
dragonhn1106 <dragonhn1106@gmail.com>
2018-01-19 17:07:13 +0700

    Init Folder giao dien

---
+++ New folder/content/js/mainscrip.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$(function(){
+  $('.bxslider').bxSlider({
+    mode: 'fade',
+    captions: false,
+    controls: false,
+    ticker : true,
+    randomStart : true,
+    speed : 5000,
+    auto : true
+  });
+});	
+})
